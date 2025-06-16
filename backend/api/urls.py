7b4b9cfa406b25df3ec2507b6a7946c8085a890f
WenXuan <wfungche@ucsc.edu>
2025-06-15 22:28:13 -0700

    Refactor Form component and pages, updated API routes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("tasks/", views.TaskListCreate.as_view(), name="task-list"),
-    path("notes/delete/<int:pk>/", views.TaskDelete.as_view(), name="delete-note")
+    path("tasks/delete/<int:pk>/", views.TaskDelete.as_view(), name="delete-task"),
 ]
