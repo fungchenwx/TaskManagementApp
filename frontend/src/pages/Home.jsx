39c410cf08ff6687a31f6bfdbe0c1bf390586379
WenXuan <wfungche@ucsc.edu>
2025-06-19 03:12:55 -0700

    reformat Home.jsx for consistent code style

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,52 +6,61 @@ function Home() {
     const [tasks, setTasks] = useState();
     const [content, setContent] = useState();
 
-    const getTasks = () => {
-        api
-            .get("/api/tasks/")
-            .then((res) => res.data)
-            .then((data) => { setTasks(data); console.log(data) })
-            .catch((err) => alert(err))
-    }
+    const getTasks = async () => {
+        try {
+            const res = await api.get("/api/tasks/");
+            const data = res.data;
+            setTasks(data);
+            console.log(data);
+        } catch (err) {
+            alert(err);
+        }
+    };
 
-    const deleteTask = (id) => {
-        api.delete(`/api/tasks/delete/${id}/`).then((res) => {
-            if (res.status === 204) alert("Task deleted!")
-            else alart("Failed to delete task")
-        })
-            .catch((error) => alert(error))
-        getTasks()
+    const deleteTask = async (id) => {
+        try {
+            const res = await api.delete(`/api/tasks/delete/${id}/`);
+            if (res.status === 204) {
+                alert("Task deleted!");
+            } else {
+                alert("Failed to delete task");
+            }
+        } catch (error) {
+            alert(error);
+        } finally {
+            getTasks();
+        }
     };
 
     const createTask = (e) => {
         e.preventDefault()
         api
-            st("/api/notes/", { content, title }).then((res) => {
-                if (res.status === 201) alert("Task created")
-                else alart("Failed to create task")
-            })
+        st("/api/notes/", { content, title }).then((res) => {
+            if (res.status === 201) alert("Task created")
+            else alart("Failed to create task")
+        })
             .catch((error) => alert(error))
         getTasks();
     };
 
 
-return <div>
-    <div>
-        <h2>Tasks</h2>
+    return <div>
+        <div>
+            <h2>Tasks</h2>
+        </div>
+        <h2>Create a Task</h2>
+        <form onSubmit={createTask}>
+            <label htmlFor="title">Title:</label>
+            <br />
+            <input
+                type="text"
+                id="title"
+                name="title"
+                required onChange={(e) => setTitle(e.target.value)}
+                value={title}
+            />
+        </form>
     </div>
-    <h2>Create a Task</h2>
-    <form onSubmit={createTask}>
-        <label htmlFor="title">Title:</label>
-        <br />
-        <input
-            type="text"
-            id="title"
-            name="title"
-            required onChange={(e) => setTitle(e.target.value)}
-            value={title}
-        />
-    </form>
-</div>
 }
 
 export default Home
