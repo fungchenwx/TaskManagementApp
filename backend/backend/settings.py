4d30fc22802d775d985e8bc83d419c3503c501dd
Ubuntu <ubuntu@ip-172-31-15-108.us-east-2.compute.internal>
2025-07-08 17:49:15 +0000

    changed allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-zbig23vtm9f&$s5y=hsf8&ldoaic=8299hq47ee+7tphk#17c="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["3.133.110.237", "localhost", "127.0.0.1"]
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
