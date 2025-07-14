7c8ace4142e8ee5a1970e613ef9f2a68057cbf2f
WenXuan <wfungche@ucsc.edu>
2025-07-13 18:19:26 -0700

    Added import for TokenObtainPairView and define ObtainTokenView with AllowAny permissions

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers import UserSerializer, TaskSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Task
 
@@ -33,4 +34,7 @@ class TaskDelete(generics.DestroyAPIView):
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+class ObtainTokenView(TokenObtainPairView):
     permission_classes = [AllowAny]
