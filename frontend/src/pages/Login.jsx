ed268b159c1b0395928c234f2f76b098f408a1ca
WenXuan <wfungche@ucsc.edu>
2025-07-20 16:57:18 -0700

    imported Link from react-router-dom in Login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,13 @@
 import Form from "../components/Form"
+import { Link } from "react-router-dom"
 
 function Login() {
     return (
         <div className="login-container">
             <Form route="/api/token/" method="login" />
-            <p>Don't have an account? <Link to="/register">Register</Link></p>
+            <p>
+                Don't have an account? <Link to="/register">Register</Link>
+            </p>
         </div>
     );
 }
