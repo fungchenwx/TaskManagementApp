030cbda6de40a9c9e430d5346834ec4b6593decf
WenXuan <wfungche@ucsc.edu>
2025-08-22 05:20:50 -0700

    added more contrast between background and task, changed task info display to flex and moved delete botton to the right, improved interactivity when hovering over task content

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -5,32 +5,18 @@ const STATUSES = ["green", "orange", "red"]
 const STATUSES_TEXT = [""]
 
 function Task({ task, onDelete, onStatusChange }) {
-
-    const initial = STATUSES.indexOf(task.status) >= 0 ? STATUSES.indexOf(task.status) : 0;
-
-    const [statusIndex, setStatusIndex] = useState(initial);
-
-    const handleStatusClick = () => {
-        const next = (statusIndex + 1) % STATUSES.length;
-        setStatusIndex(next);
-
-        if (onStatusChange) {
-            onStatusChange(task.id, STATUSES[next])
-        }
-    }
-
     const formattedDate = new Date(task.created_at).toLocaleDateString("en-US")
 
     return (
         <div className="task">
-            <div>
-                <p className="task-title">{task.title}</p>
+            <div className="task-info">
+                <h3 className="task-title">{task.title}</h3>
                 <p className="task-content">{task.content}</p>
                 <p className="task-date">{formattedDate}</p>
-                <button className="delete-button" onClick={() => onDelete(task.id)}>
-                    Delete
-                </button>
             </div>
+            <button className="delete-button" onClick={() => onDelete(task.id)}>
+                Delete
+            </button>
         </div>
     )
 }
